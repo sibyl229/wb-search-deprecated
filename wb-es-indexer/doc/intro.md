cfcc97013d0e13eceed064bc8fce7cc6a8bd2e29
Sibyl <sibyl@wormbase.org>
2017-03-24 20:37:33 -0400

    add lein new project to wb-es-indexer folder

---
+++ wb-es-indexer/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wb-es-indexer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ wb-es-indexer/test/wb_es_indexer/core_test.clj
@@ -0,0 +1,7 @@
+(ns wb-es-indexer.core-test
+  (:require [clojure.test :refer :all]
+            [wb-es-indexer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
