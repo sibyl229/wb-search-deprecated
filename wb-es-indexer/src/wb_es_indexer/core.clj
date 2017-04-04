93d40d5adef8501368f2b9c3de07a0757abab90b
Sibyl <sibyl@wormbase.org>
2017-04-03 22:28:40 -0400

    use only identifiers and create batch based on them

--- wb-es-indexer/src/wb_es_indexer/core.clj
+++ wb-es-indexer/src/wb_es_indexer/core.clj
@@ -34,6 +34,7 @@
   (let [f (fn [[k v]] [(format-field-name k) v])]
     ;; only apply to maps
     (walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) doc)))
+
 (defn- generate-batch-style-doc [doc]
   (let [action {:index {:_id (:id doc)
                         :_type (:type doc)}}
@@ -43,53 +44,73 @@
          (json/generate-string body)
          "\n")))
 
-(defn generate-batches [batch-size docs]
-  (->> (map generate-batch-style-doc docs)
-       (partition batch-size batch-size nil)
-       (map (fn [batched-docs]
-              (clojure.string/join "" batched-docs)))))
+;; (defn- create-batch [dbids doc-factory]
+;;   (->> (map doc-factory dbids)
+;;        (map generate-batch-style-doc)
+;;        (clojure.string/join "")))
+
+(defn create-batch [db pull-spec batch-dbids]
+  (->> batch-dbids
+       (d/pull-many db pull-spec)
+       (map format-doc)
+       (map generate-batch-style-doc)
+       (clojure.string/join "")))
+
+(defn generate-batches [batch-size doc-dbids]
+  (->> (partition batch-size batch-size nil doc-dbids)
+       ))
 
 (defn index-batch [batch]
   (elasticsearch "_bulk"))
 
+(defn format-doc [doc]
+  {:type "gene"
+   :doc (fix-field-names doc)})
+
 (defn run
-  ([docs] (run docs 10))
-  ([docs buffer-size]
-   (let [doc-batches (generate-batches 1000 docs)
-         buffer (chan buffer-size)]
-     (go (doseq [batch doc-batches]
-           (>! buffer batch))
+  ([doc-dbids] (run doc-dbids (pull-spec (d/db datomic-conn) type) 10))
+  ([doc-dbids pull-pattern] (run doc-dbids pull-pattern 10))
+  ([doc-dbids pull-pattern buffer-size]
+   (let [doc-batches (generate-batches 10 doc-dbids)
+         buffer (chan buffer-size)
+         db (d/db datomic-conn)]
+     (go (doseq [batch-dbids doc-batches]
+           ;;(>! buffer (create-batch batch-dbids doc-factory))
+           (>! buffer (create-batch db pull-pattern batch-dbids))
+           (println "push")
+           )
+         (println "done reading")
          (close! buffer))
      (go (loop []
            (if-let [batch (<! buffer)]
              ;; normal batches won't be nil
              ;; only get nil when channel is closed
              (do
+               (println "pop")
                (elasticsearch "_bulk" batch)
-               (recur))))))))
+               (recur))
+             ))))))
 
 (defn get-docs-by-type
   ([db type]
-   (get-docs-by-type db type (pull-spec db type)))
-  ([db type pull-spec]
    (->> (d/q '[:find [?e ...]
                :in $ ?ident
                :where
                [?e ?ident]]
-             db (keyword type "id"))
-        (map (fn [entity-id]
-               (assoc {}
-                      :id entity-id
-                      :type type
-                      :doc (fix-field-names (d/pull db pull-spec entity-id))))))))
+             db (keyword type "id")))))
+
+(defn- get-doc [db entity-id pull-pattern]
+  (assoc {}
+         :id entity-id
+         :type "gene"
+         :doc (fix-field-names (d/pull db pull-pattern entity-id))))
 
 (defn run-all []
-  (do
-
-    (run (take 2 (get-docs-by-type db "gene"
-                                   (pull-spec db "gene" [:gene/rnaseq :gene/ortholog :gene/other-sequence]))))
-    (run (lazy-seq '({:id 1 :type "gene" :doc {:a 1 :b 2}} {:id 2 :type "gene" :doc {"a$b" 1 :b 2}} {:id 4 :type "gene" :doc {:a 3333333}} {:id 5 :type "gene" :doc {:a 1 :b 2}}))))
-  )
+  (let [db (d/db datomic-conn)]
+    (do
+      (run
+        (take 100 (drop 30000 (get-docs-by-type db "gene")))
+        (pull-spec db "gene" [:gene/rnaseq :gene/ortholog :gene/paralog :gene/other-sequence])))))
 
 (defn -main
   "I don't do a whole lot ... yet."
