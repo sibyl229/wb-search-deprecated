2142a39b5a04669657da2bb10d9cd5b2e14e80a3
Sibyl <sibyl@wormbase.org>
2017-07-17 21:14:14 -0400

    configure index for autocompletion of go-term label

--- wb-es/src/wb_es/mappings/core.clj
+++ wb-es/src/wb_es/mappings/core.clj
@@ -4,11 +4,17 @@
   {:properties
    {:wbid {:type "string"
            :index "not_analyzed"}
-    :label {:type "string"}}})
+    :label {:type "string"
+            :analyzer "autocomplete"
+            :search_analyzer "standard"}}})
 
 (def index-settings
   {:settings
    {:number_of_shards 1
-    :analysis {:filter {}
-               :analyzer {}}}
+    :analysis {:filter {"autocomplete_filter" {:type "edge_ngram"
+                                               :min_gram 2
+                                               :max_gram 20}}
+               :analyzer {"autocomplete" {:type "custom"
+                                          :tokenizer "standard"
+                                          :filter ["autocomplete_filter"]}}}}
    :mappings {:go_term go-term-mapping}})
