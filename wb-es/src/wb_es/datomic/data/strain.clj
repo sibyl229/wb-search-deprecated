8593747ac0734e2156c7cc423551ef0799ea1f16
Sibyl <sibyl@wormbase.org>
2017-07-21 12:20:39 -0400

    fix strain file not committed

---
+++ wb-es/src/wb_es/datomic/data/strain.clj
@@ -0,0 +1,14 @@
+;; Genotype
+;; species
+(ns wb-es.datomic.data.strain
+  (:require [datomic.api :as d]
+            [wb-es.datomic.data.util :as data-util]))
+
+(deftype Strain [entity]
+  data-util/Document
+  (metadata [this] (data-util/default-metadata entity))
+  (data [this]
+    {:wbid (:strain/id entity)
+     :label (:strain/id entity)
+     :description (:strain/genotype entity)
+     :species (data-util/format-species-enum (:strain/species entity))}))
