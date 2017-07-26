dc398fa8139af2c9be94cc2b82b5fc9967a9a95e
Sibyl <sibyl@wormbase.org>
2017-07-26 18:27:21 +0000

    add catalyst integration routes stubs

--- wb-es/src/wb_es/web/index.clj
+++ wb-es/src/wb_es/web/index.clj
@@ -5,18 +5,38 @@
             [ring.middleware.json :refer [wrap-json-response]]
             [ring.util.response :refer [response not-found]]
             [wb-es.web.core :as web-core]))
+(def search-route
+  (GET "/search" [q & options]
+       (response (web-core/search q options))))
+
+(def autocomplete-route
+  (GET "/autocomplete" [q & options]
+       (response (web-core/autocomplete q options))))
+
+(def search-exact-route
+  (GET "/search-exact" [q & options]
+       (response (web-core/search-exact q options))))
+
+(def count-route
+  (GET "/count" [q & options]
+       (response (web-core/count q options))))
+
+(def random-route
+  (GET "/random" [q & options]
+       (response (web-core/random options))))
 
 (defroutes app
-  (GET "/search" [q & options]
-       (response (web-core/search q options)))
-  (GET "/autocomplete" [q & options]
-       (response (web-core/autocomplete q options)))
-  (GET "/search-exact" [q & options]
-       (response (web-core/search-exact q options)))
-  (GET "/count" [q & options]
-       (response (web-core/count q options)))
-  (GET "/random" [q & options]
-       (response (web-core/random options)))
+  search-route
+  autocomplete-route
+  search-exact-route
+  count-route
+  random-route
+  (context "/integration" []
+           search-route
+           autocomplete-route
+           search-exact-route
+           count-route
+           random-route)
   (route/not-found (response {:message "endpoint not found"})))
 
 (defn handler [request]
