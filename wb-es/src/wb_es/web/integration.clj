ad48ffffc38ce70c8c1e129106af599a6c0180ef
Sibyl <sibyl@wormbase.org>
2017-07-27 20:13:19 +0000

    add search catalyst integration

--- wb-es/src/wb_es/web/integration.clj
+++ wb-es/src/wb_es/web/integration.clj
@@ -7,8 +7,37 @@
      :label (:label doc-source)
      :taxonomy (:species doc-source)}))
 
+(defn- get-obj [doc]
+  (let [search-obj (pack-search-obj doc)
+        doc-source (:_source doc)]
+    {:name search-obj
+     :description (:description doc-source)
+     :taxonomy nil}))
+
 (defn wrap-search [handler]
-  handler)
+  (fn [request]
+    (let [params (:params request)
+          page (get params :page 1)
+          page_size (get params :page_size 10)
+          params-new (assoc params
+                            :size page_size
+                            :from (->> page
+                                       (Integer.)
+                                       (dec)
+                                       (* page_size)))
+          request-new (assoc request :params params-new)
+          response (handler request-new)
+          body-new {
+                    :matches (->> (get-in response [:body :hits :hits])
+                                  (map get-obj))
+                    :query (:q params)
+                    :querytime (->> (get-in response [:body :took])
+                                    (Integer.)
+                                    (#(/ % 1000.0)))
+                    :count (get-in response [:body :hits :total])
+                    :page page
+                    :page_size page_size}]
+      (assoc response :body body-new))))
 
 (defn wrap-autocomplete [handler]
   handler)
@@ -19,7 +48,7 @@
           content-new (->> (get-in response [:body :hits :hits])
                            (map pack-search-obj)
                            (first))]
-      (do (prn request) (assoc response :body content-new)))))
+      (assoc response :body content-new))))
 
 (defn wrap-count [handler]
   handler)
