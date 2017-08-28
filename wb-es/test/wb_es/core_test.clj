82dbce2caa2ded67b8c0648b4cc900e3ef922d8f
Sibyl <sibyl@wormbase.org>
2017-08-28 16:07:06 -0400

    organize tests

--- wb-es/test/wb_es/core_test.clj
+++ wb-es/test/wb_es/core_test.clj
@@ -54,58 +54,28 @@
 
 (deftest clone-type-test
   (let [db (d/db datomic-conn)]
-    (do
-      (index-datomic-entity (d/entity db [:clone/id "W02C12"]))
-      (testing "search by clone WBID"
-        (let [first-hit (->> (search "W02C12")
-                             :hits
-                             :hits
-                             (first))]
-          (is (= "W02C12" (get-in first-hit [:_source :wbid])))
-          (is (= "clone" (get-in first-hit [:_type])))))
-      (testing "autocomplete by clone WBID"
-        (let [hits (->> (autocomplete "W02C")
-                        :hits
-                        :hits)]
-          (is (some (fn [hit]
-                      (= "W02C12" (get-in hit [:_source :wbid])))
-                    hits)))
-        ))
+    (testing "clone type using W02C12 as example"
+      (do
+        (index-datomic-entity (d/entity db [:clone/id "W02C12"]))
+        (testing "search by clone WBID"
+          (let [first-hit (->> (search "W02C12")
+                               :hits
+                               :hits
+                               (first))]
+            (is (= "W02C12" (get-in first-hit [:_source :wbid])))
+            (is (= "clone" (get-in first-hit [:_type])))))
+        (testing "autocomplete by clone WBID"
+          (let [hits (->> (autocomplete "W02C")
+                          :hits
+                          :hits)]
+            (is (some (fn [hit]
+                        (= "W02C12" (get-in hit [:_source :wbid])))
+                      hits)))
+          )))
     ))
 
-(deftest transgene-type-test
-  (let [db (d/db datomic-conn)
-        transgenes-prefixed-syis1 (->> (d/datoms db :aevt :transgene/public-name)
-                                       (keep (fn [[e _ v]]
-                                               (if (re-matches #"syIs1.*" v)
-                                                 e)))
-                                       (map (partial d/entity db))
-                                       (shuffle))]
-    (do
-      (apply index-datomic-entity transgenes-prefixed-syis1)
-      (testing "autocompletion by transgene name"
-        (let [hits (-> (autocomplete "syIs1")
-                       (get-in [:hits :hits]))]
-          (testing "result appears in autocompletion"
-            (is (some (fn [hit]
-                        (= "syIs101"
-                           (get-in hit [:_source :label])))
-                      hits)))
-          (testing "result in right order"
-            (is (= "syIs1" (-> (first hits)
-                               (get-in [:_source :label])))))))
-      (testing "autocompletion by transgene name in lowercase"
-        (let [hits (-> (autocomplete "syis1")
-                       (get-in [:hits :hits]))]
-          (is (some (fn [hit]
-                      (= "syIs101"
-                         (get-in hit [:_source :label])))
-                    hits))))
-      ))
-  )
-
 (deftest disease-type-test
-  (testing "autocomplete by disease name using \"park\" as an example"
+  (testing "disease type using \"park\" as an example"
     (let [db (d/db datomic-conn)
           disease-parks (->> (d/datoms db :aevt :do-term/name)
                              (keep (fn [[e _ v]]
@@ -113,25 +83,62 @@
                                        e)))
                              (map (partial d/entity db))
                              (shuffle))]
-      (do
-        (apply index-datomic-entity disease-parks)
-        (let [hits (-> (autocomplete "park" {:size (count disease-parks)})
-                       (get-in [:hits :hits]))]
+      (testing "autocomplete by disease name"
+        (do
+          (apply index-datomic-entity disease-parks)
+          (let [hits (-> (autocomplete "park" {:size (count disease-parks)})
+                         (get-in [:hits :hits]))]
 
-          (testing "match Parkinson's disease"
-            (some (fn [hit]
-                    (= "Parkinson's disease"
-                       (get-in hit [:_source :label])))
-                  hits))
-          (testing "matching early-onset Parkinson disease"
-            (some (fn [hit]
-                    (= "early-onset Parkinson disease"
-                       (get-in hit [:_source :label])))
-                  hits))
-          (testing "X-linked dystonia-parkinsonism"
-            (some (fn [hit]
-                    (= "X-linked dystonia-parkinsonism"
-                       (get-in hit [:_source :label])))
-                  hits))
+            (testing "match Parkinson's disease"
+              (some (fn [hit]
+                      (= "Parkinson's disease"
+                         (get-in hit [:_source :label])))
+                    hits))
+            (testing "matching early-onset Parkinson disease"
+              (some (fn [hit]
+                      (= "early-onset Parkinson disease"
+                         (get-in hit [:_source :label])))
+                    hits))
+            (testing "X-linked dystonia-parkinsonism"
+              (some (fn [hit]
+                      (= "X-linked dystonia-parkinsonism"
+                         (get-in hit [:_source :label])))
+                    hits))
 
-          )))))
+            ))))
+    ))
+
+(deftest transgene-type-test
+  (testing "transgene using syis1 as example"
+    (let [db (d/db datomic-conn)
+          transgenes-prefixed-syis1 (->> (d/datoms db :aevt :transgene/public-name)
+                                         (keep (fn [[e _ v]]
+                                                 (if (re-matches #"syIs1.*" v)
+                                                   e)))
+                                         (map (partial d/entity db))
+                                         (shuffle))]
+
+      (testing "autocompletion by transgene name"
+        (do
+          (apply index-datomic-entity transgenes-prefixed-syis1))
+          (let [hits (-> (autocomplete "syIs1")
+                         (get-in [:hits :hits]))]
+            (testing "result appears in autocompletion"
+              (is (some (fn [hit]
+                          (= "syIs101"
+                             (get-in hit [:_source :label])))
+                        hits)))
+            (testing "result in right order"
+              (is (= "syIs1" (-> (first hits)
+                                 (get-in [:_source :label])))))))
+
+      (testing "autocompletion by transgene name in lowercase"
+        (do
+          (apply index-datomic-entity transgenes-prefixed-syis1)
+          (let [hits (-> (autocomplete "syis1")
+                         (get-in [:hits :hits]))]
+            (is (some (fn [hit]
+                        (= "syIs101"
+                           (get-in hit [:_source :label])))
+                      hits)))))
+      )))
